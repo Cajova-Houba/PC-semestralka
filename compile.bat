59f360a2f2b52fc76283a859d2399c14ab0ef586
Cajova-Houba <tkmushroomer@gmail.com>
2014-12-07 16:18:31 +0100

    Pridan kompilovaci script pro windows. Pridan vypocet funkce v defaultnim definicnim oboru.

---
+++ compile.bat
@@ -0,0 +1,2 @@
+@echo off
+gcc *.c -lm -o main.exe
--- main.c
+++ main.c
@@ -7,32 +7,33 @@
 
 int main(int argc, char *argv[])
 {
-	int lenMax = 50, len = lenMax,i,chyba=0;
-	char vstup[len], vystup[len];
+	int lenMax = 50, len = lenMax, i, chyba=0, res_len = 201;
+	char vstup[len], vystup[len], out_name[len];
 	chrTkn *root = NULL;
 	chrTkn *postRoot = NULL;
-	double res = 0.0, x_val = 0.0;
+	double res[res_len][2], x_val = 0.0;
+	double d_min = -10.0, d_max = 10.0; /*definicni obor*/
+	double h_min = -10.0, h_max = 10.0; /*obor hodnot*/
 	
 	for (i = 0; i < len; i++)
 	{
 		vystup[i] = '\0';
 		vstup[i] = '\0';
+		out_name[i] = '\0';
 	}
 	
 	/*strcpy(vstup, "(5+(25-(5+(5-2^2)-3*2)+5)-2*5)\0");*/
 	//strcpy(vstup, "(5+5)\0");
     /* Nacitni z argumentu*/
-    if (argc > 1)
+    if (argc < 2)
     { 
-        strcpy(vstup,argv[1]);
-        if (argc > 2)
-        {
-            x_val = atof(argv[2]);
-        }
+        printf("Chyba: spatny pocet argumentu.\n");
+		exit(1);
     }
     else
     {
-        strcpy(vstup, "-2*4\0");
+        strcpy(vstup, argv[1]);
+		strcpy(out_name, argv[2]);
     }
    	printf("Vstup: %s\n",vstup);
 	/*zjisteni delky vstupu*/
@@ -64,10 +65,21 @@ int main(int argc, char *argv[])
 	printf("Postfix: ");
 	vypis(postRoot);
 	
-	/*vypocet*/
-	res = compute(postRoot,x_val);
-	printf("Vysledek: %f\n",res);
+	/*Vypocet v D=<d_min;d_max>*/
+	x_val = d_min;
+	for (i = 0; i < res_len; i++)
+	{
+		res[i][0] = x_val;
+		res[i][1] = compute(postRoot, x_val);
+		x_val += 0.1;
+	}
 	
+	/*Vypsani vysledku*/
+	printf("x %6s\n","f(x)");
+	for(i =0; i < res_len; i++)
+	{
+		printf("%.1f %5.2f\n",res[i][0],res[i][1]);
+	}
 	smaz(root);
 	smaz(postRoot);
 
